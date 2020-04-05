a00d161e8cb01fb22a05a087134148b5d536cbcc
rezaalamsyah <53548427+rezaalamsyah@users.noreply.github.com>
2020-04-06 00:16:59 +0700

    Add files via upload

---
+++ challenge/tampilan/1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="http://localhost/challenge/tampilan/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/challenge/tampilan/fontawesome/css/all.min.css">
+
+    <title>DATA NILAI</title>
+</head>
+<body>
+
+   <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <a class="navbar-brand" href="#"><i class="fas fa-sort-amount-down"></i>Data Nilai</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="http://localhost/challenge/index.php">Data</a>
+      <a class="nav-item nav-link" href="http://localhost/challenge/siswa/tambah.php">tambah data</a>
+    </div>
+  </div>
+  &copy; by reza
+  </div>
+</nav>
---
+++ challenge/tampilan/2.php
@@ -0,0 +1,4 @@
+<script src="http://localhost/challenge/tampilan/jquery.js"></script>
+<script src="http://localhost/challenge/tampilan/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
